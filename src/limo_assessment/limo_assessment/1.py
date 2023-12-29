760c52c8119173bbbd89535bb56f6a64736632ca
patenro <chukwufagbenro@gmail.com>
2023-12-29 22:22:59 +0000

    2

--- src/limo_assessment/limo_assessment/1.py
+++ src/limo_assessment/limo_assessment/1.py
@@ -6,68 +6,112 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from nav_msgs.msg import OccupancyGrid
 from cv_bridge import CvBridge
-from ultralytics import YOLO
+
 
 class ColorMappingNode(Node):
     def __init__(self):
         super().__init__('color_mapping_node')
-        self.bridge = CvBridge()
-        self.yolo_model = YOLO('yolov8n.pt')  # Adjust the model file accordingly
 
-        # Publisher for the combined occupancy grid map
-        self.map_publisher = self.create_publisher(
-            OccupancyGrid,
-            "/map_combined",
+        # Initialize the centroid tracker and other variables
+        self.ct = cv2.TrackerCSRT_create()
+        self.colors = {'red': (0, 0, 255), 'green': (0, 255, 0), 'blue': (255, 0, 0)}
+        self.color_counts = {color: 0 for color in self.colors.keys()}
+
+        # Subscribe to the camera image topic
+        self.image_subscription = self.create_subscription(
+            Image,
+            "/limo/depth_camera_link/image_raw",
+            self.image_callback,
             10
         )
 
+        # Publisher for the occupancy grid map
+        self.map_publisher = self.create_publisher(
+            OccupancyGrid,
+            "/map_new",
+            10
+        )
+
+        self.bridge = CvBridge()
+
     def image_callback(self, msg):
+        # Convert the ROS Image message to a BGR image
         frame = self.bridge.imgmsg_to_cv2(msg, "bgr8")
 
-        # Detect colors using YOLOv8
-        results = self.yolo_model(frame)
+        # Convert the frame to HSV color space
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        # Process YOLOv8 results and create occupancy grid
-        occupancy_grid = self.process_yolo_results(results, frame)
+        # Initialize the sum total
+        sum_total = 0
 
-        # Publish the combined occupancy grid map
+        # Detect colors in the frame
+        for color, rgb in self.colors.items():
+            lower = np.array([150, 50, 50], dtype=np.uint8)
+            upper = np.array([170, 255, 255], dtype=np.uint8)
+            
+            mask = cv2.inRange(hsv, lower, upper)
+            contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            contours_count = 0
+            contours_area = []
+            for contour in contours:
+                cv2.drawContours(frame, [contour], -1, (0, 255, 0), 2)
+
+                area = cv2.contourArea(contour)
+                contours_area.append(area)
+
+                M = cv2.moments(contour)
+                if M["m00"] != 0:
+                    cX = int(M["m10"] / M["m00"])
+                    cY = int(M["m01"] / M["m00"])
+                else:
+                    cX, cY = 0, 0
+                cv2.putText(
+                    frame,
+                    f"{contours_count}",
+                    (cX - 25, cY - 25),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (255, 255, 255),
+                    2,
+                )
+
+                contours_count += 1
+
+        # Publish the occupancy grid map
+        occupancy_grid = self.create_occupancy_grid(frame)
         self.map_publisher.publish(occupancy_grid)
 
-    def process_yolo_results(self, results, frame):
-        # Initialize an empty occupancy grid map
+        # Display the frame
+        cv2.imshow("Frame", frame)
+        cv2.waitKey(1)
+
+        # Print the sum total
+        print("Sum Total:", sum_total)
+
+    def create_occupancy_grid(self, frame):
+        # Get the dimensions of the frame
         height, width, _ = frame.shape
+
+        # Create an empty occupancy grid map
         occupancy_map = np.zeros((height, width), dtype=np.int8)
 
-        # Dictionary to track unique colors
-        unique_colors = {}
+        # Iterate over each pixel in the frame
+        for y in range(height):
+            for x in range(width):
+                pixel = frame[y, x]
+                for color, rgb in self.colors.items():
+                    if np.array_equal(pixel, rgb):
+                        occupancy_map[y, x] = 100  # Set cell as occupied
+                        self.color_counts[color] += 1  # Increment color count
+                        break
+                else:
+                    occupancy_map[y, x] = -1  # Set cell as unknown
 
-        for result in results.xyxy:
-            class_id = int(result[-1])
-            color_name = self.get_color_name(class_id)  # Implement this function
-            if color_name is not None:
-                # Update color counts
-                unique_colors[color_name] = unique_colors.get(color_name, 0) + 1
-
-                # Get bounding box coordinates
-                x, y, w, h = result[:-1].cpu().numpy().astype(int)
-
-                # Draw bounding box on the frame
-                cv2.rectangle(frame, (x, y), (w, h), (0, 255, 0), 2)
-
-                # Convert bounding box to occupancy grid coordinates
-                x_center = (x + w) // 2
-                y_center = (y + h) // 2
-
-                # Set corresponding cell in the occupancy grid as occupied
-                occupancy_map[y_center, x_center] = 100
-
-        # Print color counts
-        print("Color Counts:", unique_colors)
-
-        # Create OccupancyGrid message
+        # Create an OccupancyGrid message
         occupancy_grid = OccupancyGrid()
+        occupancy_grid.header.stamp = self.get_clock().now().to_msg()
         occupancy_grid.header.frame_id = "map"
-        occupancy_grid.info.resolution = 0.1  # Adjust the resolution as needed
+        occupancy_grid.info.resolution = 0.05  # Adjust as needed
         occupancy_grid.info.width = width
         occupancy_grid.info.height = height
         occupancy_grid.info.origin.position.x = 0.0
@@ -77,37 +121,28 @@ class ColorMappingNode(Node):
         occupancy_grid.info.origin.orientation.y = 0.0
         occupancy_grid.info.origin.orientation.z = 0.0
         occupancy_grid.info.origin.orientation.w = 1.0
-        occupancy_grid.data = occupancy_map.flatten().tolist()
+
+        # Convert the occupancy map to a 1D array
+        occupancy_data = occupancy_map.flatten().tolist()
+        occupancy_grid.data = occupancy_data
 
         return occupancy_grid
 
-    def get_color_name(self, class_id):
-        # Implement this function to map YOLO class IDs to color names
-        # You can use a predefined mapping or train YOLO with specific colors
-        # For simplicity, let's assume you have a mapping dictionary named class_mapping
-        class_mapping = {
-            0: 'red',
-            1: 'green',
-            2: 'blue'
-            # Add more color mappings as needed
-        }
-        return class_mapping.get(class_id)
+    def run(self):
+        rclpy.spin(self)
+
+        # Print the color counts
+        for color, count in self.color_counts.items():
+            print(f"{color}: {count}")
+
 
 def main(args=None):
     rclpy.init(args=args)
     color_mapping_node = ColorMappingNode()
-
-    # Subscribe to the camera image topic
-    color_mapping_node.create_subscription(
-        Image,
-        "/limo/depth_camera_link/image_raw",
-        color_mapping_node.image_callback,
-        10
-    )
-
-    rclpy.spin(color_mapping_node)
+    color_mapping_node.run()
     color_mapping_node.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
